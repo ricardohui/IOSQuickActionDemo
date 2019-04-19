623be3f638460d5e83a1715265641ff06504dfaa
Ricardo Hui <eehaha@gmail.com>
2019-04-19 08:15:41 +0800

    Quick Action done

--- Quick Actions Demo/AppDelegate.swift
+++ Quick Actions Demo/AppDelegate.swift
@@ -40,7 +40,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == "com.zappycode.Quick-Actions-Demo.addnewuser" {
+            // Add a user
+        }
+        if shortcutItem.type == "com.zappycode.Quick-Actions-Demo.play" {
+            // Play
+        }
+    }
 
 }
 
